a519bde7aacbe9caa22eb2d051be6d68fcf67706
caseyjoneal <caseyjoneal@gmail.com>
2016-10-30 14:16:41 -0700

    established condition

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
-// wait for the DOM to finish loading
+console.log('you should see this');
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 
+var isX = true;
+var makeMark;
+
+$('#board').on('click', function(event) {
+    if (isX) {
+        makeMark = 'X';
+    } else {
+        markeMark = 'O';
+    }
+    isX = !isX;
+    $(event.target).html(makeMark);
+    // console.log(makeMark);
+    // evaluate();
 });
+
+
+
+
+}); // document closer
